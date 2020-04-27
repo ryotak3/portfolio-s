0ebde596b02b6e50b580386d079977c2407902d3
komatsu <ryotak3@gmail.com>
2020-04-27 17:06:02 +0900

    ジャンル別のタブを作成、UIのみ

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,68 @@
 <template>
   <section id="myworks" class="section">
     <!-- Title -->
-    <div class="section-heading">
+    <div class="section-heading block">
       <h4 class="title is-2">My works</h4>
     </div>
-    <div class="columns is-mobile is-centered">
-      <div class="column is-two-thirds">
-        <card
-          v-for="(post, i) in posts"
-          :id="post.sys.id"
-          :key="i"
-          :title="post.fields.title"
-          :date="post.sys.updateAt | moment('YYYY-MM-DD')"
-          :imgsrc="post.fields.image.fields.file.url"
-        />
-      </div>
-    </div>
+    <b-tabs v-model="activeTab">
+      <b-tab-item label="風景">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-two-thirds">
+            <card
+              v-for="(post, i) in posts"
+              :id="post.sys.id"
+              :key="i"
+              :title="post.fields.title"
+              :date="post.sys.updateAt | moment('YYYY-MM-DD')"
+              :imgsrc="post.fields.image.fields.file.url"
+            />
+          </div>
+        </div>
+      </b-tab-item>
+
+      <b-tab-item label="ライブ">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-two-thirds">
+            <card
+              v-for="(post, i) in posts"
+              :id="post.sys.id"
+              :key="i"
+              :title="post.fields.title"
+              :date="post.sys.updateAt | moment('YYYY-MM-DD')"
+              :imgsrc="post.fields.image.fields.file.url"
+            />
+          </div>
+        </div>
+      </b-tab-item>
+      <b-tab-item label="ブライダル">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-two-thirds">
+            <card
+              v-for="(post, i) in posts"
+              :id="post.sys.id"
+              :key="i"
+              :title="post.fields.title"
+              :date="post.sys.updateAt | moment('YYYY-MM-DD')"
+              :imgsrc="post.fields.image.fields.file.url"
+            />
+          </div>
+        </div>
+      </b-tab-item>
+      <b-tab-item label="ストリートスナップ">
+        <div class="columns is-mobile is-centered">
+          <div class="column is-two-thirds">
+            <card
+              v-for="(post, i) in posts"
+              :id="post.sys.id"
+              :key="i"
+              :title="post.fields.title"
+              :date="post.sys.updateAt | moment('YYYY-MM-DD')"
+              :imgsrc="post.fields.image.fields.file.url"
+            />
+          </div>
+        </div>
+      </b-tab-item>
+    </b-tabs>
   </section>
 </template>
 
@@ -47,3 +94,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.block {
+  margin: 2rem;
+}
+</style>
